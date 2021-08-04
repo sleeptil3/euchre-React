742ccffc90e53af07e1c47666c68fc1d65f0b282
sclary110 <sclary@icloud.com>
2021-08-03 22:26:21 -0400

    something is up fix in the morn

--- euchre-react-app/src/App.js
+++ euchre-react-app/src/App.js
@@ -1,9 +1,16 @@
+import { Route, Switch, useHistory } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Game from "./Pages/Game";
+import Home from "./Pages/Home";
+
 
 export default function App() {
 	return (
-		<div className="w-screen">
-			<Game />
-		</div>
+		<Switch>
+			<div className="w-screen">
+				<Route exact path="/" componenet={Game} />
+				<Route path="/game" component={Game} />
+			</div>
+		</Switch>
 	);
 }
