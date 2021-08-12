a03ed99657a5e1bd23b927edbc5e9847fa0dc36a
Shawn Clary <sclary@icloud.com>
2021-08-12 11:56:27 -0400

    minor updates

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,15 @@
-import { Link } from 'react-router-dom';
-import Header from '../Components/Header';
+import { Link } from "react-router-dom";
+import Header from "../Components/Header";
 
 export default function Home() {
 	return (
 		<div className="h-screen bg-gray-800 flex flex-col justify-start items-center">
 			<Header />
 			<div className="text-gray-50 text-8xl animate-pulse h-full w-full flex justify-center items-center">
-				<Link className="hover:text-blue-500" to="/game">Play Game</Link>
+				<Link className="hover:text-blue-500" to="/game">
+					Play Game
+				</Link>
 			</div>
 		</div>
-	)
+	);
 }
